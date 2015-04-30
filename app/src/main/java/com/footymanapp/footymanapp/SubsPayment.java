934b642d0fcad8c4a4b0a9ab99abf3e1fb46f313
keithtuffy <keithtuffy@live.com>
2015-04-30 14:52:42 +0100

    add back to subs payment

--- app/src/main/java/com/footymanapp/footymanapp/SubsPayment.java
+++ app/src/main/java/com/footymanapp/footymanapp/SubsPayment.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -88,6 +90,30 @@ public class SubsPayment extends ActionBarActivity {
                 }
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_backbutton, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_back) {
+            SubsPayment.this.finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
